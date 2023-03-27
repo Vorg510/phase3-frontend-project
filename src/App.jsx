d4d114213662a523129368b8404f43abe50946f8
Yuferril <yuferril.yunos@gmail.com>
2023-03-27 23:01:33 +1100

    update UI design

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,44 @@
-import * as React from "react";
+import React from "react";
 // import logo from './logo.svg';
 import "./App.css";
-import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 function App() {
   const [priority, setPriority] = React.useState("");
-  console.log(priority);
+  const [todo, setToDo] = React.useState("");
+  const [list, setList] = React.useState([]);
+  const handleTodo = (input) => {
+    console.log("input", input);
+    let updateList = [...list];
+    updateList.push(input);
+    console.log("updateList", updateList);
+    setList(updateList);
+  };
+  console.log("list", list);
+  console.log(todo);
+
+  React.useEffect(() => {
+    fetch("http://localhost:9292")
+      .then((r) => r.json())
+      .then((data) => console.log("data", data));
+  }, []);
 
   return (
     <div className="App">
       <h1>To Do List</h1>
       <Stack spacing={2} direction="row">
-        <TextField id="standard-basic" label="Add To-do" variant="standard" />
+        <TextField
+          id="standard-basic"
+          label="Add To-do"
+          variant="standard"
+          onChange={(e) => setToDo(e.target.value)}
+        />
         <FormControl FormControl fullWidth>
           <InputLabel id="select-label">Priority</InputLabel>
           <Select
@@ -33,14 +53,18 @@ function App() {
             <MenuItem value={"low"}>Low Priority</MenuItem>
           </Select>
         </FormControl>
-        <Button variant="contained">submit</Button>
-        </Stack>
-
-        <div>
-          <h2>My To-do</h2>
-        </div>
+        <Button variant="contained" onClick={() => handleTodo(todo)}>
+          submit
+        </Button>
+      </Stack>
+      <div>
+        <h2>My To-do</h2>
+        {list.map((item) => {
+          console.log("list2", item);
+          return <p>{item}</p>;
+        })}
       </div>
-
+    </div>
   );
 }
 
